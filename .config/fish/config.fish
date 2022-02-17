5fe48f123f0da4d40f26aa1e2e70d6ede4229c5d
Agneay B Nair <86396212+agneay@users.noreply.github.com>
2022-02-17 10:58:10 +0530

    Add files via upload

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+#starting starship prompt
+starship init fish | source
+
+#creating a random colorscript
+colorscript -r
